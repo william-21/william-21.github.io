4cf23ff5594e568f1df30902aff9fe8e890a14ba
william-21 <68625452+william-21@users.noreply.github.com>
2023-06-14 22:07:32 +0800

    Add files via upload

---
+++ william-21.github.io/1.js
@@ -0,0 +1,15 @@
+var canvas=document.getElementById("canvas");
+             var context=canvas.getContext("2d");
+             context.fillStyle="#FF66FF";
+             context.beginPath();
+             context.arc(760,300,100,0,Math.PI*2,true);
+             context.closePath();
+             context.fill();
+             function msg()
+  {
+  alert("还不能退");
+  }
+  
+
+            
+           
